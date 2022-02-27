4cd2af7aef0cb3991fdd941befad6e76b8e8f521
quangcito <qnguyen2@macalester.edu>
2022-02-26 18:39:54 -0600

    commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Nguyễn Đương Quang on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+}
+
