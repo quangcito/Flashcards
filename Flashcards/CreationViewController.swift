424487aff326a2869be256d89f9a2d69b970536d
quangcito <qnguyen2@macalester.edu>
2022-03-18 15:38:13 -0500

    work in progress

---
+++ Flashcards/CreationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Nguyễn Đương Quang on 3/18/22.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
