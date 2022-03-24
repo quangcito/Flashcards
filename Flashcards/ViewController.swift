f544f4e97cf2a7493dc6cc4e7ba54b414a372229
quangcito <qnguyen2@macalester.edu>
2022-03-24 18:53:18 -0500

    update nextprevbutton

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -17,13 +17,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     
     // Array to hold our flashcards,
     var flashcards = [Flashcard]()
     
+    // Current flashcard index
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateFlashcard(question: "What is the capital of Brazil?", answer: "Brasilia")
     }
 
     
@@ -35,6 +42,13 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func didTapOnPrev(_ sender: Any) {
+    }
+    
+    @IBAction func didTapOnNext(_ sender: Any) {
+    }
+    
+    
     func updateFlashcard(question: String, answer: String) {
         let flashcard = Flashcard(question: <#T##String#>, answer: <#T##String#>)
         frontLabel.text = flashcard.question
@@ -43,6 +57,34 @@ class ViewController: UIViewController {
         
         // Adding flashcard in the flashcards array
         flashcards.append(flashcard)
+        
+        // Logging to the console
+        print("Added new flashcard")
+        print("We now have \(flashcards.count) flashcards")
+        
+        // update current index
+        currentIndex = flashcards.count - 1
+        print("Our current index is \(currentIndex)")
+        
+        // Update buttons
+        updateNextPrevButtons()
+    }
+    
+    func updateNextPrevButtons() {
+        
+        // Disable next button if at the end
+        if currentIndex == flashcards.count - 1 {
+            nextButton.isEnabled = false
+        } else {
+            nextButton.isEnabled = true
+        }
+        
+        // Disable prev button if at the beginning
+        if currentIndex == 0 {
+            prevButton.isEnabled = false
+        } else {
+            prevButton.isEnabled = true
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
